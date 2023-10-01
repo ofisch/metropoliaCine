021eeda90db5d7afb7eb47df8751fc51fcc57c76
Lassiaa <lassiaa@metropolia.fi>
2023-10-02 01:22:55 +0300

    burger menu ja elokuva tyyli

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./node_modules/flowbite//*.js"],
   theme: {
-    fontSize: {
-      sm: "0.8rem",
-      base: "1rem",
-      xl: "1.25rem",
-      "2xl": "1.563rem",
-      "3xl": "1.953rem",
-      "4xl": "2.441rem",
-      "5xl": "3.052rem",
-    },
     extend: {
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       colors: {
         tangerine: "#e67d1c",
         gunmetal: "#292F36",
